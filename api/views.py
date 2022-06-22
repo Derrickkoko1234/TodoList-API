67957453221a16b733eb96a44edc0f20ee791484
Derrick Koko <kokoderrick3@gmail.com>
2022-06-22 01:25:13 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from yaml import serialize
 
 import api
 from api.models import Task
